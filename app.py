07f93af30f4ef31e746f1f87b042ce4315e47fb9
Kevin Zhang <kevinzhang3702@gmail.com>
2016-12-06 13:12:31 -0500

    finally got login/regis"

--- app.py
+++ app.py
@@ -5,21 +5,23 @@ app.secret_key = "SOME_KEY"
 
 @app.route("/")
 def root():
-    if( 'username' in session.keys() ):
-        return redirect(url_for( 'home' ))
+    if 'username' in session:
+        return redirect( url_for( 'home', user = session['username'] ) )
     else:
-        return redirect(url_for( 'login' ))
+        return redirect( url_for( 'home' ) )
 
 @app.route("/login/")
 def login( **keyword_parameters ):
     message = ""
-    regis = "False"
+    regis = False
     if( 'message' in keyword_parameters):
         message = keyword_parameters['message']
     elif( 'message' in request.args ):
         message = request.args.get('message')
     if( 'regis' in keyword_parameters ):
         regis = keyword_parameters['regis']
+    elif( 'regis' in request.args ):
+        regis = request.args.get('regis')
     return render_template('login.html', message = message, regis = regis)
 
 @app.route("/authenticate/", methods = ["POST"] )
@@ -42,19 +44,27 @@ def authenicate():
             else:
                 return redirect(url_for('login', message = val))
         else:
-            return redirect(url_for('login', regis = "True" ))
+            return redirect(url_for('login', regis = True ))
     else:
         return redirect(url_for( 'root' ) )
 
 @app.route("/home/")
-def home():
-    return render_template("home.html")
+def home(**keyword_parameters):
+    if( 'username' in keyword_parameters ):
+        return render_template('home.html')
+    else:
+        return render_template('home.html')
 
 
 @app.route("/account/")
 def account():
     return render_template("account.html")
-    
+
+@app.route("/logout/")
+def logout():
+    session.pop('username')
+    return redirect(url_for( 'root' ) )
+
 
 if __name__ == "__main__":
     app.debug = True
