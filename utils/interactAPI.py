d47d3548efb8573e0c48b90d4c9b64c1d5ef5992
goyyapazham <nalanda99@gmail.com>
2016-12-07 17:20:05 -0500

    search works

--- utils/interactAPI.py
+++ utils/interactAPI.py
@@ -1,13 +1,24 @@
-import json, urllib2
-from urllib2 import Request, urlopen, URLError
+import urllib2
+import json
 
-moviedb = urllib2.urlopen('https://api.themoviedb.org/3/search/movie?api_key=0c28ca3c9002dc1725a0f55971a73b4b&language=en-US&query=Avatar&page=1&include_adult=false')
-json.load(urllib2.urlopen(moviedb)
+tmdb_key="0c28ca3c9002dc1725a0f55971a73b4b"
 
+def get_ids(query):
+    url="http://api.themoviedb.org/3/search/movie?api_key=%s&query=%s&page=1"%(tmdb_key, query.replace(" ", "%20"))
+    j = json.loads(urllib2.urlopen(url).read())
+    ids = []
+    for res in j['results']:
+        ids += [res['id']]
+    return ids
 
-try:
-	response = urlopen(request)
-	movies = response.read()
-	print movies
-except URLError, e:
-    print 'Movie not found. Got an error code:', e
+def get_details(query):
+    ids = get_ids(query)
+    info = []
+    for id in ids:
+        url="http://api.themoviedb.org/3/movie/%d?api_key=%s&language=en-US"%(id, tmdb_key)
+        j = json.loads(urllib2.urlopen(url).read())
+        movie = [j['original_title'], j['release_date'][0:4]]
+        info += [movie]
+    return info
+
+print get_details("10 things")
