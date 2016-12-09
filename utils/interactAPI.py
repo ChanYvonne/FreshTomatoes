3b92f236d9d4550066799ec3d114cd96a244ea4c
ChanYvonne <ychan1352@gmail.com>
2016-12-09 08:29:32 -0500

    I fixed api bugs somehow

--- utils/interactAPI.py
+++ utils/interactAPI.py
@@ -4,7 +4,7 @@ import json
 tmdb_key="0c28ca3c9002dc1725a0f55971a73b4b"
 nyt_key="923bd15ebe684cb685e2e8f326a47ac3"
 
-def get_ids(query, type): #gets required id that allows us to access database for info on specific movie
+def get_ids(query, type): #takes search input and gets corresponding ids that match with the movie/actor
     if(type == 'm'):
         url="http://api.themoviedb.org/3/search/movie?api_key=%s&query=%s&page=1"%(tmdb_key, query.replace(" ", "%20"))
     else:
@@ -16,7 +16,7 @@ def get_ids(query, type): #gets required id that allows us to access database fo
     return ids
 
 
-def get_search_details_m(ids): #takes movie search input and returns the movies that are related to the search
+def get_search_details_m(ids): #takes movie search ids and returns the corresponding movies
     info = []
     for id in ids:
         url="http://api.themoviedb.org/3/movie/%d?api_key=%s&language=en-US"%(id, tmdb_key)
@@ -26,7 +26,7 @@ def get_search_details_m(ids): #takes movie search input and returns the movies 
     return info
 
 
-def get_search_details_a(ids): #takes name of actor as search input and returns the movies he/she is in or affiliated with
+def get_search_details_a(ids): #takes actor ids and returns the movies he/she is in or affiliated with
     for id in ids:
         url="https://api.themoviedb.org/3/person/%d/movie_credits?api_key=%s&language=en-US"%(id, tmdb_key)
         j = json.loads(urllib2.urlopen(url).read())
