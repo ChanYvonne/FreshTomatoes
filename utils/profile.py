0a1230cbefe2f2c595ec6d5a7fc7ec5396aa7a31
ChanYvonne <ychan1352@gmail.com>
2016-12-13 08:38:22 -0500

    began work on profile

---
+++ utils/profile.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+database = sqlite3.connect('data/database.db')
+c = database.cursor()
+
+def favGenre(user): #returns user's favorite genre
+
+
+def makeGenre(id, user): #allows user to change favorite genre
+
+
+def favMovie(user): #returns user's favorite movie
+
+
+def makeMovie(id, user): #allows user to change favorite movie
+    
--- utils/storage.py
+++ utils/storage.py
@@ -3,7 +3,7 @@ import sqlite3
 database = sqlite3.connect('data/database.db')
 c = database.cursor()
 
-def movieExists(id, user):
+def movieExists(id, user): #checks if a movie exists in the database
     c.execute('SELECT * FROM ' + user)
     b = c.fetchall()
     for r in b:
@@ -11,11 +11,11 @@ def movieExists(id, user):
             return True
     return False
 
-def addMovie(id, user):
+def addMovie(id, user): #adds movie to database
     if (not movieExists(id, user)):
         c.execute("INSERT INTO " + user + " VALUES (" + str(id) + ")")
         database.commit()
 
-def getMovies(user):
+def getMovies(user): #returns all movies for a specific user
     c.execute('SELECT * FROM ' + user)
     return c.fetchall()
